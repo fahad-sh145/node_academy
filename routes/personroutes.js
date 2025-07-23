0b6e164e142927fce11448f114cfb58ad80981f1
fahad-sh145 <bs6816828@gmail.com>
2025-07-24 04:13:58 +0530

    Fix person.js casing issue for Linux (Render) deployment

--- routes/personroutes.js
+++ routes/personroutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const person =require('../models/Person');
+const person =require('../models/person');
 
 
 router.post('/',async(req ,res)=>{
